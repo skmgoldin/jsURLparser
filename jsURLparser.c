358838cbc578f28c54d3ecc8bca4957e8845b677
Mike Goldin <skmgoldin@gmail.com>
2014-11-08 22:01:14 -0500

    Silenced a compiler warning.

--- jsURLparser.c
+++ jsURLparser.c
@@ -3,32 +3,31 @@
 int collapsestring(char *string, int x);
 
 int main(int argc, char **argv) {
+
   if(argc > 2) {
     perror("Only submit one URL, please.");
   }
 
   char *url = *(argv + 1);
-  printf("%s\n", url);
 
   char b;
   int x;
   for(x = 0; (b = *(url + x)) != '\0'; x++) {
-    printf("%s\n", "yes");
     if(b == '\\') {
-      printf("%s\n", "no");
       collapsestring(url, x);
     }
   }
 
+  printf("%s\n", url);
+
   return 0;
 }
 
-// I'll need some recursion up in this eventually.
-
 int collapsestring(char *string, int x) {
-  for(x = x; *(string + x) != '\0'; x++) {
+
+  for(; *(string + x) != '\0'; x++) {
     *(string + x) = *(string + (x + 1));
   }
-  printf("%s%s\n", "Collapse: ", string);
+
   return 0;
 }
